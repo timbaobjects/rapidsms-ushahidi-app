f1fe56e93d63d370f0d0ba4f3909424fb35e3933
Tim Akinbo <takinbo@timbaobjects.com>
2011-10-23 09:44:03 +0100

    documentation updates and functionality completion

--- README.rst
+++ README.rst
@@ -18,8 +18,17 @@ Here's an example configuration that's included in a typical settings.py of a Ra
     USHAHIDI_KEYWORD = 'report'
     
     USHAHIDI_RESPONSE = 'Thank you for your report'
+    
+    USHAHIDI_ERROR = "Sorry we couldn't process your report at this time. Please try sending it again."
+    
+    USHAHIDI_TRIGGER_URL = 'http://beta1-1.ushahididev.com/frontlinesms/?key=TWKJHBID&s=${sender_number}&m=${message_content}'
 
 The first setting sets the keyword to look out for to activate the application. In this example, the 
 keyword ``report`` is used. Any messages beginning with that keyword is handled by the Ushahidi application.
 
 The second setting configures the response that would be sent for messages processed by the Ushahidi application.
+
+The third setting allows you to determine what error message gets sent back to the user when an error occurs in attempting to process the message.
+
+The fourth setting configures the Ushahidi FrontlineSMS HTTP Post link. After configuring Ushahidi to accept messages from FrontlineSMS, 
+you'll get a HTTP Post link that you copy and paste for this setting.
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import re
+from urllib import urlopen, quote_plus
 from django.conf import settings
 from rapidsms.apps.base import AppBase
 
@@ -6,12 +7,19 @@ class App(AppBase):
     def start(self):
         keyword = getattr(settings, 'USHAHIDI_KEYWORD', '')
         self.default_response = getattr(settings, 'USHAHIDI_RESPONSE', 'Thank you for your report.')
+        self.error_response = getattr(settings, 'USHAHIDI_ERROR', "Due to some error, we're unable to process your message. Please resend.")
         self.pattern = re.compile(r"^\s*(?:%s)(?:[\s,;:]+(.+))?$" % (keyword))
+        self.trigger_url = re.sub('\$\{sender_number\}', '%(sender)s', getattr(settings, 'USHAHIDI_TRIGGER_URL'))
+        self.trigger_url = re.sub('\$\{message_content\}', '%(message)s', self.trigger_url)
     
     def handle(self, msg):
         match = self.pattern.match(msg.text)
         if match is None:
             return False
         else:
-            # TODO: POST message to Ushahidi
+            trigger_url = self.trigger_url % ({'message': quote_plus(msg.text), 'sender': quote_plus(msg.peer)})
+            try:
+                urlopen(trigger_url)
+            except IOError:
+                return msg.error(self.error_response)
             return msg.respond(self.default_response)
